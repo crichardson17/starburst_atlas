fdf9806331c304eb568fc01e59837da5ec952098
HELEN MESKHIDZE <emeskhidze@elon.edu>
2016-01-20 18:06:47 -0500

    edits to JWST plot

--- JWST_Comparison/Baseline_plotter2sub.py
+++ JWST_Comparison/Baseline_plotter2sub.py
@@ -105,7 +105,7 @@ def add_sub_plot(sub_num, elinesplot):
 	plt.scatter(max_values[line[elinesplot][sub_num-1],2], max_values[line[elinesplot][sub_num-1],3], c ='k',marker = '*')
 	plt.annotate(headers[line[elinesplot][sub_num-1]], xy=(8,11),  xytext=(7,9.5), fontsize = 10)
 
-	plt.annotate("Dust-free Subsolar (0.2 M$\odot$)", xy=(8,11),  xytext=(1.4,8.5), fontsize = 10)
+	plt.annotate("Dust-free 0.2 Z$\odot$", xy=(8,11),  xytext=(4.85,8.5), fontsize = 10)
 
 	plt.annotate(max_values[line[elinesplot][sub_num-1],0], xy = (max_values[line[elinesplot][sub_num-1],2], max_values[line[elinesplot][sub_num-1],3]), 
 		xytext = (0, -10), textcoords = 'offset points', ha = 'right', va = 'bottom', fontsize=10, color='k')	
